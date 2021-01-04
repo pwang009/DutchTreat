12e1a78364ac9976c3915a4c3e7caf817db4f9f2
Tony Wang <pwang009@hotmail.com>
2021-01-04 11:01:13 -0800

    Add project files.

---
+++ DutchTreat/Program.cs
@@ -0,0 +1,45 @@
+﻿using DutchTreat.Data;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DutchTreat
+{
+    public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = BuildWebHost(args);
+
+      SeedDb(host);
+
+      host.Run();
+    }
+
+    private static void SeedDb(IWebHost host)
+    {
+      var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+      using (var scope = scopeFactory.CreateScope())
+      {
+        var seeder = scope.ServiceProvider.GetService<DutchSeeder>();
+        seeder.SeedAsync().Wait();
+      }
+    }
+    
+    public static IWebHost BuildWebHost(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(SetupConfiguration)
+            .UseStartup<Startup>()
+            .Build();
+
+    private static void SetupConfiguration(WebHostBuilderContext ctx, IConfigurationBuilder builder)
+    {
+      // Removing the default configuration options
+      builder.Sources.Clear();
+
+      builder.AddJsonFile("config.json", false, true)
+             .AddEnvironmentVariables();
+    }
+  }
+}
---
+++ DutchTreat/Startup.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using DutchTreat.Data;
+using DutchTreat.Data.Entities;
+using DutchTreat.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Reflection;
+using System.Text;
+
+namespace DutchTreat
+{
+    public class Startup
+  {
+    private readonly IConfiguration _config;
+
+    public Startup(IConfiguration config)
+    {
+      _config = config;
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddIdentity<StoreUser, IdentityRole>(cfg =>
+      {
+        cfg.User.RequireUniqueEmail = true;
+      })
+        .AddEntityFrameworkStores<DutchContext>();
+
+      services.AddAuthentication()
+        .AddCookie()
+        .AddJwtBearer(cfg =>
+        {
+          cfg.TokenValidationParameters = new TokenValidationParameters()
+          {
+            ValidIssuer = _config["Tokens:Issuer"],
+            ValidAudience = _config["Tokens:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]))
+          };
+
+        });
+
+      services.AddDbContext<DutchContext>(cfg =>
+      {
+        cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+      });
+
+      services.AddAutoMapper(Assembly.GetEntryAssembly());
+
+      services.AddTransient<DutchSeeder>();
+
+      services.AddScoped<IDutchRepository, DutchRepository>();
+
+      services.AddTransient<IMailService, NullMailService>();
+      // Support for real mail service
+      
+      services.AddControllersWithViews();
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      app.UseNodeModules();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      app.UseRouting();
+
+      app.UseEndpoints(cfg =>
+      {
+        cfg.MapControllerRoute("Default",
+              "{controller}/{action}/{id?}",
+              new { controller = "App", Action = "Index" });
+      });
+
+
+    }
+  }
+}
